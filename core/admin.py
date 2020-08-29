da97cbac507f198ffd19b2ab88108e85c30f8005
tchoupoGuy <42350606+tchoupoGuy@users.noreply.github.com>
2020-08-28 22:29:25 -0400

    add user models

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+
+from core import models
+
+
+class UserAdmin(BaseUserAdmin):
+    ordering = ['id']
+    list_display = ['email', 'name']
+
+
+admin.site.register(models.User, UserAdmin)
